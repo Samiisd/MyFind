0451192175734570bbc1ee52a3de38c3400b7b6b
Sami Issaadi <sami.issaadi@epita.fr>
2018-10-09 16:05:07 +0200

    add parsing func

--- src/expr_parser.c
+++ src/expr_parser.c
@@ -1,11 +1,21 @@
 #include "expr_parser.h"
 
-static int expr_parse_rec(struct stream st, struct operand_list op_l, int mbp)
+static const char *peek(struct stream *st)
+{
+    return st->expr[st->index];
+}
+
+static const char *next(struct stream *st)
+{
+    return st->expr[st->index++];
+}
+
+static int expr_parse_rec(struct stream *st, struct operand_list op_l, int mbp)
 {
 
 }
 
-int expr_parse(struct stream st, struct operand_list op_l)
+int expr_parse(struct stream *st, struct operand_list op_l)
 {
 
 }
--- src/expr_parser.h
+++ src/expr_parser.h
@@ -5,27 +5,27 @@ enum associativity
 {
     left,
     right
-}
+};
 
 struct operand
 {
     const char *str;
     int bp; /* Binding Power */
     int asso; /* Associativity */
-};
+    };
 
-struct stream
-{
-    const char *expr[];
-    size_t index;
-};
+    struct stream
+    {
+        const char *expr[];
+        size_t index;
+    };
 
-struct operand_list
-{
-    const struct operand *list;
-    size_t size;
-};
+    struct operand_list
+    {
+        const struct operand *list;
+        size_t size;
+    };
 
-int expr_parse(struct stream st, struct operand_list op_l);
+    int expr_parse(struct stream *st, struct operand_list op_l);
 
 #endif /* EXPR_PARSER_H */
