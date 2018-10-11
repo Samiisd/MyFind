bbdb011e9a2832017500ca360c9dd54336f7fe14
Sami Issaadi <samiisd@hotmail.fr>
2018-10-11 19:48:40 +0200

    Parser now unable to handle consts + Errors checks
    When an unknown expression is parsed inside expression, no matter it is
    a constant integer, an error is launched.
    
    No idea on how handling constant integers and handling errors at the same time
    now. However it should not cause any problems in parsing "find(1)"
    expressions.

--- src/parser/expression.c
+++ src/parser/expression.c
@@ -14,14 +14,27 @@ Expression(int bp):
 
 int expression(int mbp)
 {
-    int left_ctx = tok_next()->nud();
+    const struct token *curr = tok_next();
+    if (!curr || curr->type != EXPRESSION)
+        errx(1, "'%s' is not a valid expression", 
+             tok_at_str(tok_cur_index() - 1));
 
-    const struct token *curr = tok_peek();
+    int left_ctx = curr->nud();
+
+    curr = tok_peek();
+    if (curr->type != EXPRESSION)
+        errx(1, "'%s' is not a valid expression",
+             tok_at_str(tok_cur_index() - 1));
+
     while (curr && curr->bp > mbp)
     {
         curr = tok_next();
         left_ctx = curr->led(left_ctx);
         curr = tok_peek();
+
+        if (curr->type != EXPRESSION)
+            errx(1, "'%s' is not a valid expression",
+                 tok_at_str(tok_cur_index() - 1));
     }
 
     return left_ctx;
