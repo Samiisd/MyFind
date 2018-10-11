564d7bc6da0bd384580d552e21fc23e7fa529965
Sami Issaadi <samiisd@hotmail.fr>
2018-10-11 16:03:54 +0200

    Tokenizer now need a default token + Code cleanup
    This has been made to handle constants

--- src/tokenizer/tokparsing.c
+++ src/tokenizer/tokparsing.c
@@ -5,6 +5,8 @@ struct tok_stream
     char **arr;
     size_t size;
     size_t index;
+
+    const struct token *tok_def;
 };
 
 static struct tok_stream stream =
@@ -12,25 +14,35 @@ static struct tok_stream stream =
     0
 };
 
-void tok_start(char **splitted_str, size_t nb_str)
+void tok_start(char **splitted_str, size_t nb_str, const struct token *tok_def)
 {
     stream.arr = splitted_str;
     stream.size = nb_str;
     stream.index = 0;
+    stream.tok_def = tok_def;
 }
 
+static const struct token *tok_get(size_t index)
+{
+    const struct token *found = tok_find_token(stream.arr[index]);
+
+    if (!found)
+        return stream.tok_def;
+
+    return found;
+}
 const struct token *tok_peek(void)
 {
-    if (stream.index == stream.size)
+    if (stream.index + 1 >= stream.size)
         return NULL;
 
-    return tok_find_token(stream.arr[stream.index + 1]);
+    return tok_get(stream.index + 1);
 }
 
 const struct token *tok_next(void)
 {
-    if (stream.index == stream.size)
+    if (stream.index + 1 >= stream.size)
         return NULL;
 
-    return tok_find_token(stream.arr[stream.index++]);
+    return tok_get(stream.index++);
 }
