97fc0a05e54024d1c3150be76a95d3b603d2acd3
Sami Issaadi <samiisd@hotmail.fr>
2018-10-12 19:30:10 +0200

    add function to make easier tree construction

--- src/expression/tree.c
+++ src/expression/tree.c
@@ -2,6 +2,20 @@
 
 #include <stdlib.h>
 
+struct tree_node *tree_make(void *data, struct tree_node *left,
+                            struct tree_node *right)
+{
+    struct tree_node *res = malloc(sizeof(struct tree_node));
+    if (!res)
+        return NULL;
+
+    res->data = data;
+    res->left = left;
+    res->right = right;
+
+    return res;
+}
+
 void tree_free(struct tree_node *root, void (*free_fct)(void*))
 {
     if (!root)
--- src/expression/tree.h
+++ src/expression/tree.h
@@ -9,6 +9,8 @@ struct tree_node
     struct tree_node *right;
 };
 
+struct tree_node *tree_make(void *data, struct tree_node *left,
+                            struct tree_node *right);
 void tree_free(struct tree_node *root, void (*free_fct)(void*));
 
 #endif /* TREE_H */
